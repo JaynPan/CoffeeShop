183dfd87b21093577c44636ca01deaa68a4614ff
JaynPan <lb0104672@gmail.com>
2018-06-17 13:26:46 -0700

    Coffee Shop
    This is the business brochure group project!

---
+++ scripts/hamburger.js
@@ -0,0 +1,11 @@
+const $hamburger = $('.hamburger-menu');
+const $bar = $('.bar');
+const $body = $('body');
+const $mobile = $('.container');
+
+$hamburger.click(showMenu);
+function showMenu(){
+    $body.toggleClass('show');
+    $mobile.fadeToggle();
+    $bar.toggleClass('animate');
+}
